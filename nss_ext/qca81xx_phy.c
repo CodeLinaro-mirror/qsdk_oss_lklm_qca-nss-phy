d77be1e5733ae2853897971c62440a682f6739b1
zhongjia <quic_zhongjia@quicinc.com>
2024-09-24 17:44:30 +0800

    [qca-nss-phy] fix the link issue for long cable
    Change-Id: Ifbb59793e561a0a713566061926b7a48d6bae925
    Signed-off-by: zhongjia <quic_zhongjia@quicinc.com>
    (cherry picked from commit fd84aeaef4574895794aae1e31e0db3c6a6950f4)

--- nss_ext/qca81xx_phy.c
+++ nss_ext/qca81xx_phy.c
@@ -74,6 +74,28 @@ static int qca81xx_phy_cdt(struct nss_phy_device *nss_phydev, u32 mdi_pair,
 	enum nss_phy_cable_status *status, u32 *cable_len)
 {
 	int ret;
+	u16 dac8_tmp, dac9_tmp;
+
+	/* if PHY link up, cdt status is noarmal and cable length is 0 */
+	if (nss_phydev_link_get(nss_phydev) != NSS_PHY_FALSE) {
+		*status = CABLE_NORMAL;
+		*cable_len = 0;
+		return 0;
+	}
+
+	dac8_tmp = nss_phy_read_debug(nss_phydev,
+		QCA81XX_PHY_DEBUG_AFE_DAC8_DP);
+	dac9_tmp = nss_phy_read_debug(nss_phydev,
+		QCA81XX_PHY_DEBUG_AFE_DAC9_DP);
+
+	ret = nss_phy_write_debug(nss_phydev,
+		QCA81XX_PHY_DEBUG_AFE_DAC8_DP, 0);
+	if (ret < 0)
+		return ret;
+	ret = nss_phy_write_debug(nss_phydev,
+		QCA81XX_PHY_DEBUG_AFE_DAC9_DP, 0);
+	if (ret < 0)
+		return ret;
 
 	ret = nss_phy_c45_common_cdt_start(nss_phydev);
 	if (ret < 0)
@@ -82,6 +104,14 @@ static int qca81xx_phy_cdt(struct nss_phy_device *nss_phydev, u32 mdi_pair,
 	/* Get cable status */
 	ret = nss_phy_common_cdt_status_get(nss_phydev, mdi_pair, status,
 		cable_len);
+
+	/* recover the analog AFE DAC value */
+	ret = nss_phy_write_debug(nss_phydev,
+		QCA81XX_PHY_DEBUG_AFE_DAC8_DP, dac8_tmp);
+	if (ret < 0)
+		return ret;
+	ret = nss_phy_write_debug(nss_phydev,
+		QCA81XX_PHY_DEBUG_AFE_DAC9_DP, dac9_tmp);
 
 	return ret;
 }
