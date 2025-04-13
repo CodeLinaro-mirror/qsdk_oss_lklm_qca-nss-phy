5232b52fcb07509a191bf77c18bfc07681039b17
Zhongjian Liu <zhongjia@qti.qualcomm.com>
2025-04-13 09:19:37 +0800

    [qca-nss-phy] add new files for htt PHY driver
    Change-Id: Icfad3f82998c019806ff913b86b9db8fb7bb82dc
    Signed-off-by: zhongjia <zhongjia@qti.qualcomm.com>

---
+++ linux_std/qce1204/qce1204.h
@@ -0,0 +1,13 @@
+/*
+* Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+* SPDX-License-Identifier: ISC
+*/
+
+#ifndef _QCE1204_
+#define _QCE1204_
+
+#include <linux/phy.h>
+
+#define QCE1204_PHY		0x004dd190
+
+#endif /* _QCE1204_H_ */
