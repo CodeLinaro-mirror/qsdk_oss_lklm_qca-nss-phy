90c88f5d2cd9e0b2cea360b6cb8d955f43ad4be2
Zhongjian Liu <zhongjia@qti.qualcomm.com>
2025-04-13 09:22:33 +0800

    [qca-nss-phy] enable PHY basic OPS of htt PHY
    Change-Id: Ifaa9f848cb62a3d4e2a379e5c6d5d2363d79a109
    Signed-off-by: zhongjia <zhongjia@qti.qualcomm.com>

--- linux_std/qce1204/qce1204.c
+++ linux_std/qce1204/qce1204.c
@@ -5,13 +5,226 @@
 
 #include "qce1204.h"
 
+#define QCE1204_PHY_SPEC_STATUS						0x11
+#define QCE1204_PHY_SS_LINK_STATUS					0x400
+#define QCE1204_PHY_INTR_DOWNSHIFT					0x20
+#define QCE1204_PHY_SS_DUPLEX_FULL					0x2000
+#define QCE1204_PHY_SS_SPEED_MASK					0x380
+#define QCE1204_PHY_SS_SPEED_2500					0x200
+#define QCE1204_PHY_SS_SPEED_1000					0x100
+#define QCE1204_PHY_SS_SPEED_100					0x80
+#define QCE1204_PHY_SS_SPEED_10						0
+#define QCE1204_PHY_INTR_MASK						0x12
+
+#define QCE1204_PHY_INTR_STATUS						0x13
+#define QCE1204_PHY_INTR_STATUS_DOWN					0x800
+#define QCE1204_PHY_INTR_STATUS_UP					0x400
+#define QCE1204_PHY_1000BASET_CONTROL					0x9
+#define QCE1204_PHY_ADVERTISE_1000FULL					0x200
+#define QCE1204_PHY_1000BASET_STATUS					0xa
+#define QCE1204_PHY_LP_ADVERTISE_1000FULL				0x800
+#define QCE1204_PHY_CONTROL						0x19
+#define QCE1204_PHY_FIFO_RESET						0x3
+#define QCE1204_PHY_MMD7_IPG_OP						0x901d
+#define QCE1204_PHY_IPG_10_TO_11_EN					BIT(0)
+
+int qce1204_phy_config_aneg(struct phy_device *phydev)
+{
+	bool changed = false;
+	u16 reg = 0;
+	int ret = 0;
+
+	if (phydev->autoneg == AUTONEG_DISABLE) {
+		int duplex_val = BMCR_FULLDPLX, duplex_tmp = DUPLEX_FULL;
+		/* genphy_c45_pma_setup_forced only support duplex full, */
+		/* so need to set duplex as full to configure speed */
+		/* when duplex is half */
+		duplex_tmp = phydev->duplex;
+		if(phydev->duplex == DUPLEX_HALF) {
+			phydev->duplex = DUPLEX_FULL;
+			duplex_val = 0;
+		}
+		ret = genphy_c45_pma_setup_forced(phydev);
+		if (ret < 0)
+			return ret;
+		phydev->duplex = duplex_tmp;
+		return phy_modify_mmd(phydev, MDIO_MMD_VEND2, MII_BMCR,
+			BMCR_FULLDPLX, duplex_val);
+	}
+
+	ret = genphy_c45_an_config_aneg(phydev);
+	if (ret < 0)
+		return ret;
+	if (ret > 0)
+		changed = true;
+
+	/* Clause 45 has no standardized support for 1000BaseT, */
+	/* therefore use vendor registers. */
+	if (linkmode_test_bit(ETHTOOL_LINK_MODE_1000baseT_Full_BIT,
+		phydev->advertising))
+		reg |= QCE1204_PHY_ADVERTISE_1000FULL;
+	ret = phy_modify_mmd_changed(phydev, MDIO_MMD_VEND2,
+		QCE1204_PHY_1000BASET_CONTROL,
+		QCE1204_PHY_ADVERTISE_1000FULL,
+		reg);
+	if (ret < 0)
+		return ret;
+	if (ret > 0)
+		changed = true;
+
+	return genphy_c45_check_and_restart_aneg(phydev, changed);
+}
+
+int qce1204_phy_soft_reset(struct phy_device *phydev)
+{
+	return phy_modify_mmd(phydev, MDIO_MMD_VEND2,
+		MII_BMCR, BMCR_RESET, BMCR_RESET);
+}
+
+int qce1204_phy_ack_interrupt(struct phy_device *phydev)
+{
+	int ret = 0;
+
+	ret = phy_read_mmd(phydev, MDIO_MMD_VEND2,
+		QCE1204_PHY_INTR_STATUS);
+
+	return (ret < 0) ? ret : 0;
+}
+
+int qce1204_phy_config_intr(struct phy_device *phydev)
+{
+	int ret = 0;
+	u16 mask = 0;
+
+	mask = QCE1204_PHY_INTR_STATUS_DOWN | QCE1204_PHY_INTR_STATUS_UP;
+
+	if (phydev->interrupts == PHY_INTERRUPT_ENABLED) {
+		ret = qce1204_phy_ack_interrupt(phydev);
+		if (ret < 0)
+			return ret;
+		ret = phy_modify_mmd(phydev, MDIO_MMD_VEND2, QCE1204_PHY_INTR_MASK,
+			mask, mask);
+	} else {
+		ret = phy_modify_mmd(phydev, MDIO_MMD_VEND2, QCE1204_PHY_INTR_MASK,
+			mask, 0);
+		if (ret < 0)
+			return ret;
+		ret = qce1204_phy_ack_interrupt(phydev);
+	}
+
+	return ret;
+}
+
+irqreturn_t qce1204_phy_handle_interrupt(struct phy_device *phydev)
+{
+	int irq_status, int_enabled;
+
+	irq_status = phy_read_mmd(phydev, MDIO_MMD_VEND2, QCE1204_PHY_INTR_STATUS);
+	if (irq_status < 0) {
+		phy_error(phydev);
+		return IRQ_NONE;
+	}
+
+	/* Read the current enabled interrupts */
+	int_enabled = phy_read_mmd(phydev, MDIO_MMD_VEND2, QCE1204_PHY_INTR_MASK);
+	if (int_enabled < 0) {
+		phy_error(phydev);
+		return IRQ_NONE;
+	}
+
+	/* See if this was one of our enabled interrupts */
+	if (!(irq_status & int_enabled))
+		return IRQ_NONE;
+
+	phy_trigger_machine(phydev);
+
+	return IRQ_HANDLED;
+}
+
 static int qce1204_phy_probe(struct phy_device *phydev)
 {
+	struct device *dev = &phydev->mdio.dev;
+	int ret;
+
+	ret = devm_of_phy_package_join(dev, phydev, 0);
+
+	return ret;
+}
+
+static int qce1204_ability_fix_up(struct phy_device *phydev)
+{
+
+	if (phydev->interface != PHY_INTERFACE_MODE_INTERNAL &&
+		phydev->interface != PHY_INTERFACE_MODE_GMII) {
+		linkmode_clear_bit(ETHTOOL_LINK_MODE_10baseT_Half_BIT,
+			phydev->supported);
+		linkmode_clear_bit(ETHTOOL_LINK_MODE_100baseT_Half_BIT,
+			phydev->supported);
+		linkmode_clear_bit(ETHTOOL_LINK_MODE_10baseT_Half_BIT,
+			phydev->advertising);
+		linkmode_clear_bit(ETHTOOL_LINK_MODE_100baseT_Half_BIT,
+			phydev->advertising);
+	}
+
 	return 0;
 }
 
 static int qce1204_phy_config_init(struct phy_device *phydev)
 {
+	return qce1204_ability_fix_up(phydev);
+}
+
+int qce1204_phy_read_status(struct phy_device *phydev)
+{
+	int ret = 0;
+
+	ret = genphy_c45_read_status(phydev);
+	if (ret < 0)
+		return ret;
+
+	if (phydev->autoneg == AUTONEG_ENABLE) {
+		ret = phy_read_mmd(phydev, MDIO_MMD_VEND2,
+			QCE1204_PHY_1000BASET_STATUS);
+		if (ret < 0)
+			return ret;
+		linkmode_mod_bit(ETHTOOL_LINK_MODE_1000baseT_Full_BIT,
+			phydev->lp_advertising,
+			ret & QCE1204_PHY_LP_ADVERTISE_1000FULL);
+		/* need to get speed and duplex again with new lp adv */
+		phy_resolve_aneg_linkmode(phydev);
+	} else {
+		ret = phy_read_mmd(phydev, MDIO_MMD_VEND2, MII_BMCR);
+		if (ret < 0)
+			return ret;
+		phydev->duplex = (ret & BMCR_FULLDPLX) ? DUPLEX_FULL : DUPLEX_HALF;
+	}
+
+	ret = phy_read_mmd(phydev, MDIO_MMD_VEND2, QCE1204_PHY_SPEC_STATUS);
+	if (ret < 0)
+		return ret;
+	if (ret & QCE1204_PHY_INTR_DOWNSHIFT) {
+		switch (ret & QCE1204_PHY_SS_SPEED_MASK) {
+		case QCE1204_PHY_SS_SPEED_2500:
+			phydev->speed = SPEED_2500;
+			break;
+		case QCE1204_PHY_SS_SPEED_1000:
+			phydev->speed = SPEED_1000;
+			break;
+		case QCE1204_PHY_SS_SPEED_100:
+			phydev->speed = SPEED_100;
+			break;
+		case QCE1204_PHY_SS_SPEED_10:
+			phydev->speed = SPEED_10;
+			break;
+		default:
+			phydev->speed = SPEED_UNKNOWN;
+		}
+		if (ret & QCE1204_PHY_SS_DUPLEX_FULL)
+			phydev->duplex = DUPLEX_FULL;
+		else
+			phydev->duplex = DUPLEX_HALF;
+	}
+
 	return 0;
 }
 
@@ -22,8 +235,13 @@ static struct phy_driver qce1204_phy_driver[] = {
 	.flags = PHY_POLL_CABLE_TEST,
 	.probe = qce1204_phy_probe,
 	.config_init = qce1204_phy_config_init,
+	.config_aneg = qce1204_phy_config_aneg,
+	.config_intr = qce1204_phy_config_intr,
+	.handle_interrupt = qce1204_phy_handle_interrupt,
+	.read_status = qce1204_phy_read_status,
 	.suspend = genphy_c45_pma_suspend,
 	.resume = genphy_c45_pma_resume,
+	.soft_reset = qce1204_phy_soft_reset,
 },
 };
 
