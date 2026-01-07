f802328b00d8113cc4fcc01548b3e9ad67028e63
zhongjia <zhongjia@qti.qualcomm.com>
2026-01-07 23:36:03 +0800

    [qca-nss-phy] remove some reset from PHY init
    Change-Id: I075af701000ecb09e7b3d24f1d772bba4a107dc4
    Signed-off-by: zhongjia <zhongjia@qti.qualcomm.com>

--- linux_std/qca81xx/qca81xx.c
+++ linux_std/qca81xx/qca81xx.c
@@ -1017,9 +1017,6 @@ static int qca81xx_pcs_usxgmii_init(struct phy_device *phydev)
 	ret = qca81xx_xpcs_clk_reset_update(phydev, false);
 	if (ret < 0)
 		return ret;
-	ret = qca81xx_phy_soft_reset(phydev);
-	if (ret < 0)
-		return ret;
 	ret = qca81xx_pcs_modify_mmd(phydev,
 		MDIO_MMD_PCS, QCA81XX_PCS_MMD3_PCS_CTRL2,
 		QCA81XX_PCS_MMD3_PCS_TYPE_MASK,
@@ -1343,11 +1340,35 @@ static int qca81xx_phy_ana_capacitance_update(struct phy_device *phydev)
 	/* adjust the training duration */
 	ret = phy_modify_mmd(phydev, MDIO_MMD_PCS, QCA81XX_MMD3_TRAIN_DURATION_CTRL,
 		QCA81XX_MMD3_DURATION_MASK, QCA81XX_MMD3_DURATION_VAL);
-	if (ret < 0)
-		return ret;
-	ret = qca81xx_phy_soft_reset(phydev);
 
 	return ret;
+}
+
+static int qca81xx_phy_suspend(struct phy_device *phydev)
+{
+	int ret;
+
+	ret = phy_read_mmd(phydev, MDIO_MMD_VEND2,
+		QCA81XX_SPEC_STATUS);
+	if (!(ret & QCA81XX_SS_LINK_STATUS)) {
+		ret = qca81xx_phy_pcs_assert(phydev, true);
+		if (ret < 0)
+			return ret;
+	}
+
+	return genphy_c45_pma_suspend(phydev);
+}
+
+static int qca81xx_phy_resume(struct phy_device *phydev)
+{
+	int ret;
+
+	/* make sure the PHY PCS is enabled */
+	ret = qca81xx_phy_pcs_assert(phydev, false);
+	if (ret < 0)
+		return ret;
+
+	return genphy_c45_pma_resume(phydev);
 }
 
 static int qca81xx_phy_config_init(struct phy_device *phydev)
@@ -1415,6 +1436,10 @@ static int qca81xx_phy_config_init(struct phy_device *phydev)
 		return ret;
 
 	ret = qca81xx_phy_ana_capacitance_update(phydev);
+	if (ret < 0)
+		return ret;
+
+	ret = qca81xx_phy_resume(phydev);
 	if (ret < 0)
 		return ret;
 
@@ -1785,6 +1810,7 @@ static int qca81xx_phy_probe(struct phy_device *phydev)
 	device_create_file(&phydev->mdio.dev, &dev_attr_snr);
 	/* to fix the reboot issue of laguna SFP */
 	phydev->drv->mdiodrv.driver.shutdown = qca81xx_phy_shutdown;
+	qca81xx_phy_suspend(phydev);
 
 	return 0;
 }
@@ -1792,33 +1818,6 @@ static int qca81xx_phy_probe(struct phy_device *phydev)
 static void qca81xx_phy_remove(struct phy_device *phydev)
 {
 	device_remove_file(&phydev->mdio.dev, &dev_attr_snr);
-}
-
-static int qca81xx_phy_suspend(struct phy_device *phydev)
-{
-	int ret;
-
-	ret = phy_read_mmd(phydev, MDIO_MMD_VEND2,
-		QCA81XX_SPEC_STATUS);
-	if (!(ret & QCA81XX_SS_LINK_STATUS)) {
-		ret = qca81xx_phy_pcs_assert(phydev, true);
-		if (ret < 0)
-			return ret;
-	}
-
-	return genphy_c45_pma_suspend(phydev);
-}
-
-static int qca81xx_phy_resume(struct phy_device *phydev)
-{
-	int ret;
-
-	/* make sure the PHY PCS is enabled */
-	ret = qca81xx_phy_pcs_assert(phydev, false);
-	if (ret < 0)
-		return ret;
-
-	return genphy_c45_pma_resume(phydev);
 }
 
 int qca81xx_phy_set_wol(struct phy_device *phydev,
@@ -2585,7 +2584,6 @@ static struct phy_driver qca81xx_phy_driver[] = {
 	.read_status = qca81xx_phy_read_status,
 	.suspend = qca81xx_phy_suspend,
 	.resume = qca81xx_phy_resume,
-	.soft_reset = qca81xx_phy_soft_reset,
 	.set_wol = qca81xx_phy_set_wol,
 	.get_wol = qca81xx_phy_get_wol,
 	.get_tunable = qca81xx_phy_get_tunable,
